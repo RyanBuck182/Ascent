75296f3b8c95a9a873a160b5ad6c8acf7e1eba7c
ryanbuck182 <ryanbuck182@gmail.com>
2023-12-21 16:50:29 -0500

    Removed space

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -110,7 +110,6 @@ async function displayDivider() {
 /** Updates pseudo console font sizes. Runs when the page is resized. */
 function updateFontSizes() {
     let lines = PseudoConsole.lines();
-
     for (let i = 0; i < lines.length; i++)
         lines[i].style.fontSize = PseudoConsole.pseudoConsoleFontSize();
 }
